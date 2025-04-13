8b3733f12be422ddac466cce95bdbdf4859f7e64
pankaj0110 <kumar.pankaj0110@gmail.com>
2025-04-13 17:01:21 +0530

    added main nav desktop

--- src/components/main-nav/MainNav.jsx
+++ src/components/main-nav/MainNav.jsx
@@ -1,16 +1,21 @@
 /**
  * All rights reserved by DhauladharVasi
  */
+import logo from '../../assets/svg/logo.svg'
 
 import classes from './MainNav.module.less';
 
 const MainNav = () => {
   return (
     <div className={classes.mainNav}>
+      <div className={classes.logoTheme}>
+        <img src={logo} className={classes.logo} alt='Dhauladhar vasi' />
+        <span className={classes.logoText}>Dhaulahar vasi</span>
+      </div>
       <div className={classes.mainNavWrapper}>
         <ul>
           <li>
-            <h1 className={classes.heading}>Dhauladhar Vasi</h1>
+            <h1 className={classes.heading}>Home</h1>
             </li>
         </ul>
       </div>
